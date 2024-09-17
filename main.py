0b6d26f4f2774aee68e8a4beb2f780d0d2dfed39
eloahbaracho <eloahbarachodossantos@gmail.com>
2024-09-17 11:18:15 -0300

    st.4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd 
 import yfinance as yf
+from datetime import timedelta
 #funções de carregamento dos dados
 #BTG, Itaú
 @st.cache_data #atribui funcionalidade (decorator) // armazenamento em cache // boa prática
@@ -22,17 +23,28 @@ dados = carregar_dados(acoes)
 st.write("""
 # Precificação de ações
 O gráfico representa a volução do preço das ações do BTG (BPAC11) ao longo dos anos
-""") #markdown com ####
+""") #markdown 
 
-#visualizações = filtros 
-lista_acoes = st.multiselect("Escolha as ações para visualizar", dados.columns) #selecionar ações
+#filtros de visualizações - sidebar
+st.sidebar.header("Filtragem")
+
+#Filtro de visualizações das ações
+lista_acoes = st.sidebar.multiselect("Escolha as ações para visualizar", dados.columns) #selecionar ações
 #filtrar da tabela dados com lista de ações
 if lista_acoes:
     dados = dados[lista_acoes]
     if len(lista_acoes) == 1: #criar compatibilidade para exibir uma única ação 
         acao_unica = lista_acoes[0]
         dados = dados.rename(columns={acao_unica: "Close"})
-    
-        
+#Filtro de datas -- data mais antiga para a mais recente
+data_inicial = dados.index.min().to_pydatetime()
+data_final = dados.index.max().to_pydatetime()
+intervalo_data = st.sidebar.slider("Selecione o período", 
+                                   min_value=data_inicial,
+                                   max_value=data_final,
+                                   value=data_final, 
+                                   
+value=(data_inicial, data_final), step=timedelta(days=1)) #value representa valor de início mostrado num intervalo de datas
+print(intervalo_data)
 #gráfico
 st.line_chart(dados)
