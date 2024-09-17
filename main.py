89e6bd6566800483cc1dddcaa327667f14adc1d5
eloahbaracho <eloahbarachodossantos@gmail.com>
2024-09-17 10:32:03 -0300

    st.3

--- main.py
+++ main.py
@@ -9,19 +9,14 @@ import yfinance as yf
 def carregar_dados(empresas):
     tickers = " ".join(empresas)
     # Formatação para "BPAC11.SA ITUB4.SA XPBR31.SA BPAN4.SA"
+    dados_acao = yf.TickerS(tickers)
+    cotacoes_acao = dados_acao.history(period="1d", start="2015-01-01", end="2024-08-01") #histórico de cotações
+    cotacoes_acao = cotacoes_acao["Close"] #printa como uma tabela do pandas até fechamento da ação
 
-    dados_acao = yf.TickerS(empresa)
-    cotacoes_acao = dados_acao.history(period="1d", start="2014-01-01", end="2024-08-01") #histórico de cotações
-    print(cotacoes_acao)
-    cotacoes_acao = cotacoes_acao[["Close"]] #printa como uma tabela do pandas até fechamento da ação
     return cotacoes_acao
 
 acoes = ["BPAC11.SA", "ITUB4.SA", "XPBR31.SA", "BPAN4.SA"]
-carregar_dados("BPAC11.SA")
-print(dados)
-
-#visualizações 
-dados = carregar_dados(acoes)
+dados = carregar_dados(acoes) 
 
 #criar interface streamlit
 st.write("""
@@ -29,5 +24,15 @@ st.write("""
 O gráfico representa a volução do preço das ações do BTG (BPAC11) ao longo dos anos
 """) #markdown com ####
 
+#visualizações = filtros 
+lista_acoes = st.multiselect("Escolha as ações para visualizar", dados.columns) #selecionar ações
+#filtrar da tabela dados com lista de ações
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes) == 1: #criar compatibilidade para exibir uma única ação 
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns={acao_unica: "Close"})
+    
+        
 #gráfico
 st.line_chart(dados)
